695d47eb6f9b4d4061838e45beb7a2ee1abcbe4b
philippelaumay <philippe.laumay@bonitasoft.com>
2018-11-26 10:23:35 +0100

    first version

---
+++ src-groovy/JobStatusChecker.groovy
@@ -0,0 +1,13 @@
+import org.bonitasoft.engine.connector.uipath.model.JobState
+
+class JobStatusChecker {
+
+	static boolean hasFailed(String state) {
+		state != null && !state.endsWith("ing") && state != JobState.SUCCESSFUL.toString()
+	}
+	
+	static boolean isSuccessful(String state) {
+		state != null &&  state == JobState.SUCCESSFUL.toString()
+	}
+	
+}
